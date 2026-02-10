af2cc7d8d1f8644d946a8d01c58a1511b0fd1de8
Vinícius Pereira <vinicius.gpereira03@gmail.com>
2026-02-10 08:22:43 -0300

    Criada interface ListaRepository

---
+++ internal/domain/interface/lista_repository.go
@@ -0,0 +1,19 @@
+package interfaces
+
+import "comparei-servico-listas/internal/domain/listas"
+
+type ListaRepository interface {
+	HasOpenList(userID string) (bool, error)
+	Create(lista *listas.Lista) (int64, error)
+
+	GetByID(id int64, userID string) (*listas.Lista, error)
+	GetAll(userID string) ([]*listas.Lista, error)
+	Update(lista *listas.Lista) error
+
+	AddItem(item *listas.ItemLista) error
+	RemoveItem(itemID int64) error
+	UpdateItem(item *listas.ItemLista) error
+	GetItem(itemID int64) (*listas.ItemLista, error)
+
+	UpdatePriceInOpenLists(produtoID int64, mercadoID int64, novoPreco float64) error
+}
